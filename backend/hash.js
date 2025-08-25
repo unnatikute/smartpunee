242526e7066a59278c586de55e829cdff594700a
Unnati Kute <144412234+unnatikute@users.noreply.github.com>
2025-08-25 21:40:36 +0530

    Delete backend directory

--- backend/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function run() {
-  const plainPassword = '1234'; // Replace with your actual password
-  const hash = await bcrypt.hash(plainPassword, 10);
-  console.log('Your hashed password:', hash);
-}
-
-run();
